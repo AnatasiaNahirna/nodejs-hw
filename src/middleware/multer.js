ec580259cb9397e78f98e4f67ed3a2231e23cc55
Anastasia <akroyl419@gmail.com>
2025-11-30 12:54:11 +0200

    add avatars

---
+++ src/middleware/multer.js
@@ -0,0 +1,15 @@
+import multer from 'multer';
+
+export const upload = multer({
+  storage: multer.memoryStorage(),
+  limits: {
+    filesize: 2 * 1024 * 1024,
+  },
+  fileFilter: (req, file, cb) => {
+    if (!file.mimetype || !file.mimetype.startsWith('image/')) {
+      return cb(new Error('only images allowed'));
+    }
+
+    cb(null, true);
+  }
+});
--- src/server.js
+++ src/server.js
@@ -10,6 +10,7 @@ import cookieParser from "cookie-parser";
 
 import authRoutes from './routes/authRoutes.js';
 import notesRoutes from "./routes/notesRoutes.js";
+import userRoutes from './routes/userRoutes.js';
 import { errors } from 'celebrate';
 
 const app = express();
@@ -20,6 +21,7 @@ app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
 
+app.use(userRoutes);
 app.use(authRoutes);
 app.use(notesRoutes);
 
